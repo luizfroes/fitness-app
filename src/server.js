0d0e1d4c26fedcb59f3a8040f5ac5a36fe37afd2
Luiz Carlos Froes <luizfroes@gmail.com>
2022-01-13 20:25:01 +0000

    installed session packges

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const expressHandlebars = require("express-handlebars");
 const path = require("path");
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./routes");
 const connection = require("./config/connection");
 
 const PORT = process.env.PORT || 4000;
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 86400 * 1000,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new sequelizeStore({
+    db: connection,
+  }),
+};
 
 const handlebars = expressHandlebars.create({});
 const app = express();
@@ -14,6 +28,7 @@ const app = express();
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
+app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
