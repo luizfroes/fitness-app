473ecb40f9e25376788a1a7835c5ba1ef8cbcb82
gingus55 <gingus55@hotmail.com>
2022-01-13 19:55:32 +0000

    get target selection

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,11 @@
 console.log("Hello, world!!");
+const targetSearch = $("#exTargetBtn");
+
+const handleExTarget = async (event) => {
+  event.preventDefault();
+  const target = $("#exTarget").val();
+
+  console.log(target);
+};
+
+targetSearch.on("click", handleExTarget);
