717596629285472271de2418798dd758e79683ad
gingus55 <gingus55@hotmail.com>
2022-01-13 18:49:16 +0000

    hooks and hashing added

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,68 +1,72 @@
 const bcrypt = require("bcrypt");
 const { Model, DataTypes } = require("sequelize");
 const connection = require("../config/connection");
-// const hooks = require("../hooks/hashPassword");
+const hooks = require("../hooks/hashPassword");
 
 class Users extends Model {
-    async checkPassword(userPassword) {
-        const isValid = await bcrypt.compare(userPassword, this.password);
-        return isValid;
-    }
+  async checkPassword(userPassword) {
+    const isValid = await bcrypt.compare(userPassword, this.password);
+    return isValid;
+  }
 }
 
-Users.init({
+Users.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     user_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true,
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [8, 20],
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 20],
+      },
     },
     age: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     weight: {
-        type: DataTypes.DECIMAL,
-        allowNull: true,
+      type: DataTypes.DECIMAL,
+      allowNull: true,
     },
     height: {
-        type: DataTypes.DECIMAL,
-        allowNull: true,
+      type: DataTypes.DECIMAL,
+      allowNull: true,
     },
-}, {
+  },
+  {
     sequelize: connection,
     timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: "users",
-});
+    hooks,
+  }
+);
 
 module.exports = Users;
