3d96f795a5678e703b055ef55070b11c4bea22c0
Darion Richards <darionrichards@outlook.com>
2022-01-14 16:07:46 +0000

    Included Helper function within a single index.js
    Helpers
    - renamed file to index.js
    - exported functions
    - fixed imports where helper functions are used

---
+++ src/helpers/index.js
@@ -0,0 +1,19 @@
+const getPayloadWithValidFieldsOnly = (validFields, payload) =>
+    Object.entries(payload).reduce(
+        (acc, [key, value]) =>
+        validFields.includes(key) ? {...acc, [key]: value } : acc, {}
+    );
+
+const logError = (type = "Server Error", message = "Something went wrong") => {
+    console.error(`[ERROR]: ${type} | ${message}`);
+};
+
+const logInfo = (type, message) => {
+    console.log(`[INFO]: ${type} | ${message}`);
+};
+
+module.exports = {
+    getPayloadWithValidFieldsOnly,
+    logError,
+    logInfo,
+};
--- src/helpers/logger.js
+++
@@ -1,9 +0,0 @@
-const logError = (type = "Server Error", message = "Something went wrong") => {
-  console.error(`[ERROR]: ${type} | ${message}`);
-};
-
-const logInfo = (type, message) => {
-  console.log(`[INFO]: ${type} | ${message}`);
-};
-
-module.exports = { logError, logInfo };
