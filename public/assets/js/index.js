859be16a81c48bacede204fa410989fbb4996290
Darion Richards <darionrichards@outlook.com>
2022-01-14 16:16:25 +0000

    Handle Signup Request
    Handlebars & CSS
    - included username field for database when creating a user
    - styled username field
    
    Controllers
    - moved Login, Signup and Logout controller functions into the AUTH controllers
    - conditionalised controller functions
    - if username already exists in database, will not create user
    - if email already exists in database, will not create user
    
    Client Side JS
    - if password < 8 characters user is alerted before submiting
    - if password > 20 character user is alerted before submitting
    - if password !== confirmedPassword user is alerted before submitting
    - if sign up is successfull user is re-directed to "/login" page and alerted that signup was a success

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,47 @@
-console.log("Hello, world!!");
+const signUpFormElement = $("#signup-form");
+
+const handleSignUp = async(event) => {
+    event.preventDefault();
+
+    const firstName = $("#first-name-input").val();
+    const lastName = $("#last-name-input").val();
+    const username = $("#username-input").val();
+    const email = $("#email-input").val();
+    const password = $("#password-input").val();
+    const confirmPassword = $("#confirm-password-input").val();
+    const age = $("#age-select :selected").text();
+
+    if (password.length < 8) {
+        alert("Password must be 8 or more characters");
+    } else if (password.length > 20) {
+        alert("Password must be 20 characters or less");
+    } else if (password !== confirmPassword) {
+        alert("Passwords do not match");
+    } else {
+        const response = await fetch("/auth/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                first_name: firstName,
+                last_name: lastName,
+                user_name: username,
+                email,
+                password,
+                age,
+            }),
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+        if (data.success) {
+            alert("Successfully created account");
+            window.location.replace("/login");
+        }
+    }
+};
+
+signUpFormElement.on("submit", handleSignUp);
--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,148 +1,47 @@
 const { Routine, Exercise, ExerciseRoutine, User } = require("../../models");
-const { logError } = require("../../helpers/logger");
-const { getPayloadWithValidFieldsOnly } = require("../../helpers/helpers.js");
 
-const login = async (req, res) => {
-  try {
-    const payload = getPayloadWithValidFieldsOnly(
-      ["email", "password"],
-      req.body
-    );
-    if (Object.keys(payload).length !== 2) {
-      return res.status(400).json({
-        success: false,
-        error: "Please provide all the valid fields in the post body!",
-      });
-    }
+const createNewRoutine = async(req, res) => {
+    res.send("createNewRoutine");
+};
 
-    const user = await User.findOne({ where: { email: payload.email } });
+const updateRoutineById = async(req, res) => {
+    res.send("updateRoutineById");
+};
 
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        error: "Please provide a valid Email!",
-      });
-    }
+const deleteRoutineById = async(req, res) => {
+    res.send("deleteRoutineById");
+};
 
-    const validPassword = await user.checkPassword(payload.password);
-
-    if (!validPassword) {
-      return res.status(401).json({
-        success: false,
-        error: "Invalid Password!",
-      });
-    }
-
-    const userInSession = {
-      id: user.get("id"),
-      email: user.get("email"),
-      firstName: `${user.get("first_name")}`,
-      lastName: `${user.get("last_name")}`,
-      age: `${user.get("age")}`,
-      weight: `${user.get("weight")}`,
-      height: `${user.get("height")}`,
-    };
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-      req.session.user = userInSession;
-
-      return res.json({ success: true, data: "Login successful" });
+const getAllRoutines = async(req, res) => {
+    const routines = await Routines.findAll({
+        include: [{
+                model: Exercises,
+                through: ExerciseRoutine,
+            },
+            {
+                model: Users,
+            },
+        ],
     });
-  } catch (error) {
-    logError("LOGIN User", error.message);
 
-    return res
-      .status(500)
-      .json({ success: false, error: "Failed to Login User" });
-  }
-};
-
-const signup = async (req, res) => {
-  try {
-    const payload = getPayloadWithValidFieldsOnly(
-      [
-        "user_name",
-        "email",
-        "password",
-        "first_name",
-        "last_name",
-        "age",
-        "weight",
-        "height",
-      ],
-      req.body
-    );
-
-    if (Object.keys(payload).length !== 8) {
-      return res.status(400).json({
-        success: false,
-        error: "Please provide all the valid fields in the post body!",
-      });
-    }
-
-    await User.create(payload);
-
-    return res.json({ success: true, data: "User successfully created" });
-  } catch (error) {
-    logError("Create User failed", error);
-
-    return res
-      .status(500)
-      .json({ success: false, error: "Failed to create User" });
-  }
-};
-
-const logout = async (req, res) => {
-  res.send("logout");
-};
-
-const createNewRoutine = async (req, res) => {
-  res.send("createNewRoutine");
-};
-
-const updateRoutineById = async (req, res) => {
-  res.send("updateRoutineById");
-};
-
-const deleteRoutineById = async (req, res) => {
-  res.send("deleteRoutineById");
-};
-
-const getAllRoutines = async (req, res) => {
-  const routines = await Routines.findAll({
-    include: [
-      {
-        model: Exercises,
-        through: ExerciseRoutine,
-      },
-      {
-        model: Users,
-      },
-    ],
-  });
-
-  const allRoutines = routines.map((each) => {
-    return each.get({
-      plain: true,
+    const allRoutines = routines.map((each) => {
+        return each.get({
+            plain: true,
+        });
     });
-  });
 
-  res.json({ data: allRoutines });
-  //res.send("getAllRoutines");
+    res.json({ data: allRoutines });
+    //res.send("getAllRoutines");
 };
 
-const getRoutineById = async (req, res) => {
-  res.send("getRoutineById");
+const getRoutineById = async(req, res) => {
+    res.send("getRoutineById");
 };
 
 module.exports = {
-  login,
-  signup,
-  logout,
-  createNewRoutine,
-  updateRoutineById,
-  deleteRoutineById,
-  getRoutineById,
-  getAllRoutines,
+    createNewRoutine,
+    updateRoutineById,
+    deleteRoutineById,
+    getRoutineById,
+    getAllRoutines,
 };
