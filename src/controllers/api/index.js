9e71c4f57219d33fb5a0c6ea1173164de3906a21
Darion Richards <darionrichards@outlook.com>
2022-01-21 00:51:55 +0000

    Conditionalise createNewRoutine controller function
    - if payload body is === "" return error message

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -2,146 +2,180 @@ const { Routine, Exercise, ExerciseRoutine, User } = require("../../models");
 const axios = require("axios").default;
 const { getPayloadWithValidFieldsOnly } = require("../../helpers");
 
-const createNewRoutine = async (req, res) => {
-  res.send("createNewRoutine");
-};
+const createNewRoutine = async(req, res) => {
+    try {
+        const { user } = req.session;
 
-const updateRoutineById = async (req, res) => {
-  const routine = req.params.target;
-  const exerRoutine = await ExerciseRoutine.findAll({
-    where: { routine_id: routine },
-  });
-  console.log(exerRoutine);
-  res.send("updateRoutineById");
-};
+        const payload = getPayloadWithValidFieldsOnly(
+            ["routineName", "routineDate", "routineStart", "routineEnd"],
+            req.body
+        );
 
-const deleteRoutineById = async (req, res) => {
-  res.send("deleteRoutineById");
-};
+        if (!payload.routineName ||
+            !payload.routineDate ||
+            !payload.routineStart ||
+            !payload.routineEnd
+        ) {
+            return res.json({
+                success: false,
+                error: "Cannot POST empty fields",
+            });
+        }
 
-const getAllRoutines = async (req, res) => {
-  const routines = await Routine.findAll({
-    include: [
-      {
-        model: Exercise,
-        through: ExerciseRoutine,
-      },
-      {
-        model: User,
-      },
-    ],
-  });
+        if (Object.keys(payload).length !== 4) {
+            return res.status(400).json({ error: "Please provide a valid request" });
+        }
 
-  const allRoutines = routines.map((each) => {
-    return each.get({
-      plain: true,
-    });
-  });
+        const data = await Routine.create({
+            date: new Date(payload.routineDate),
+            time_start: payload.routineStart,
+            time_end: payload.routineEnd,
+            name: payload.routineName,
+            user_id: user.id,
+        });
 
-  return allRoutines;
-  // res.json({ data: allRoutines });
-  //res.send("getAllRoutines");
-};
-
-const getRoutinesByUser = async (req, res) => {
-  const { user } = req.session;
-  // console.log(req.session);
-  const routines = await Routine.findAll({
-    include: [
-      {
-        model: Exercise,
-        through: ExerciseRoutine,
-      },
-      {
-        model: User,
-      },
-    ],
-    where: { user_id: user.id },
-  });
-
-  const allRoutines = routines.map((each) => {
-    return each.get({
-      plain: true,
-    });
-  });
-
-  return allRoutines;
-};
-
-const getRoutineById = async (req, res) => {
-  const routineData = await Routine.findByPk(req.params.id, {
-    include: [
-      {
-        model: Exercise,
-        through: ExerciseRoutine,
-      },
-      {
-        model: User,
-      },
-    ],
-  }).catch((err) => {
-    res.json(err);
-  });
-
-  const routine = routineData.get({ plain: true });
-
-  res.json({ data: routine });
-};
-
-const getExercisesByTarget = async (req, res) => {
-  // console.log(req);
-  const URL = `https://exercisedb.p.rapidapi.com/exercises/target/${req}`;
-
-  const { data } = await axios.get(URL, {
-    headers: {
-      "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-      "x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
-    },
-  });
-
-  const shuffled = data.sort(() => 0.5 - Math.random());
-  const selected = shuffled.slice(0, 6);
-
-  // console.log(selected);
-
-  return selected;
-};
-
-const createExercise = async (req, res) => {
-  try {
-    const payload = getPayloadWithValidFieldsOnly(
-      ["exercise_name", "image", "target", "routine_id"],
-      req.body
-    );
-
-    if (Object.keys(payload).length !== 4) {
-      return res.status(400).json({ error: "Please provide a valid request" });
+        return res.status(200).json({
+            success: true,
+            data: data,
+        });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error.message });
     }
+};
 
-    const exercise = await Exercise.create({
-      exercise_name: payload.exercise_name,
-      image: payload.image,
-      target: payload.target,
+const updateRoutineById = async(req, res) => {
+    const routine = req.params.target;
+    const exerRoutine = await ExerciseRoutine.findAll({
+        where: { routine_id: routine },
+    });
+    console.log(exerRoutine);
+    res.send("updateRoutineById");
+};
+
+const deleteRoutineById = async(req, res) => {
+    res.send("deleteRoutineById");
+};
+
+const getAllRoutines = async(req, res) => {
+    const routines = await Routine.findAll({
+        include: [{
+                model: Exercise,
+                through: ExerciseRoutine,
+            },
+            {
+                model: User,
+            },
+        ],
     });
 
-    const exerciseRoutine = await ExerciseRoutine.create({
-      routine_id: payload.routine_id,
-      exercise_id: exercise.get("id"),
+    const allRoutines = routines.map((each) => {
+        return each.get({
+            plain: true,
+        });
     });
-    return res.json({ success: true });
-  } catch (error) {
-    console.log(`[ERROR]: Failed to create exercise | ${error.message}`);
-    return res.status(500).json({ error: "Failed to create exercise" });
-  }
+
+    return allRoutines;
+    // res.json({ data: allRoutines });
+    //res.send("getAllRoutines");
+};
+
+const getRoutinesByUser = async(req, res) => {
+    const { user } = req.session;
+    // console.log(req.session);
+    const routines = await Routine.findAll({
+        include: [{
+                model: Exercise,
+                through: ExerciseRoutine,
+            },
+            {
+                model: User,
+            },
+        ],
+        where: { user_id: user.id },
+    });
+
+    const allRoutines = routines.map((each) => {
+        return each.get({
+            plain: true,
+        });
+    });
+
+    return allRoutines;
+};
+
+const getRoutineById = async(req, res) => {
+    const routineData = await Routine.findByPk(req.params.id, {
+        include: [{
+                model: Exercise,
+                through: ExerciseRoutine,
+            },
+            {
+                model: User,
+            },
+        ],
+    }).catch((err) => {
+        res.json(err);
+    });
+
+    const routine = routineData.get({ plain: true });
+
+    res.json({ data: routine });
+};
+
+const getExercisesByTarget = async(req, res) => {
+    // console.log(req);
+    const URL = `https://exercisedb.p.rapidapi.com/exercises/target/${req}`;
+
+    const { data } = await axios.get(URL, {
+        headers: {
+            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+            "x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
+        },
+    });
+
+    const shuffled = data.sort(() => 0.5 - Math.random());
+    const selected = shuffled.slice(0, 6);
+
+    // console.log(selected);
+
+    return selected;
+};
+
+const createExercise = async(req, res) => {
+    try {
+        const payload = getPayloadWithValidFieldsOnly(
+            ["exercise_name", "image", "target", "routine_id"],
+            req.body
+        );
+
+        if (Object.keys(payload).length !== 4) {
+            return res.status(400).json({ error: "Please provide a valid request" });
+        }
+
+        const exercise = await Exercise.create({
+            exercise_name: payload.exercise_name,
+            image: payload.image,
+            target: payload.target,
+        });
+
+        const exerciseRoutine = await ExerciseRoutine.create({
+            routine_id: payload.routine_id,
+            exercise_id: exercise.get("id"),
+        });
+        return res.json({ success: true });
+    } catch (error) {
+        console.log(`[ERROR]: Failed to create exercise | ${error.message}`);
+        return res.status(500).json({ error: "Failed to create exercise" });
+    }
 };
 
 module.exports = {
-  createNewRoutine,
-  updateRoutineById,
-  deleteRoutineById,
-  getRoutineById,
-  getAllRoutines,
-  getExercisesByTarget,
-  getRoutinesByUser,
-  createExercise,
+    createNewRoutine,
+    updateRoutineById,
+    deleteRoutineById,
+    getRoutineById,
+    getAllRoutines,
+    getExercisesByTarget,
+    getRoutinesByUser,
+    createExercise,
 };
