07e718db8629511d94277e9cc83bc093b25b6ddb
Darion Richards <darionrichards@outlook.com>
2022-01-21 14:45:14 +0000

    Fixed rendering problem if user is not loggedIn

--- src/controllers/view/public.js
+++ src/controllers/view/public.js
@@ -8,9 +8,13 @@ const renderLogin = (req, res) => {
 const renderHome = (req, res) => {
     const { loggedIn } = req.session;
 
-    const { firstName, lastName } = req.session.user;
+    if (!loggedIn) {
+        return res.render("home");
+    } else {
+        const { firstName, lastName } = req.session.user;
 
-    return res.render("home", { loggedIn, firstName, lastName });
+        return res.render("home", { loggedIn, firstName, lastName });
+    }
 };
 
 const renderSignUp = (req, res) => {
